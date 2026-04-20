9a9da7e15c56b27ce39e5c4906737122b13a4640
d <ddher101@gitlab.com>
2026-04-19 20:53:30 -0400

    partial

--- test/nsm_monitor.py
+++ test/nsm_monitor.py
@@ -84,6 +84,7 @@ class Monitor_Bluetooth():
         timeout = 10
         cycle = 0
         unstable_devices = []
+        panel = Panel(renderable="Developed by nsm_barii", style="bold red", border_style="bold purple", expand=False)
 
         table = Table(title="BLE Driving", title_style="bold red", border_style="bold purple", style="bold purple", header_style="bold red")
         table.add_column("#"); table.add_column("RSSI", style=c2); table.add_column("Mac", style=c3); table.add_column("Manufacturer", style=c5); table.add_column("Local_name"); table.add_column("UUID", style=c3)
@@ -92,149 +93,155 @@ class Monitor_Bluetooth():
         try:
 
             scanner = BleakScanner()
-
-            while True:
-
-
-                await scanner.start()
-                await asyncio.sleep(5)
-                await scanner.stop()
-                devices = scanner.discovered_devices_and_advertisement_data
-                now     = time.time()
-                cycle   += 1
-
-
-
-                if devices: 
-                
-                    
-                    for mac, (device, adv) in devices.items():
-                        
-                        name  = adv.local_name or False
-                        rssi  = adv.rssi
-                        uuid  = adv.service_uuids or False
-                        manuf = cls._get_manuf(manuf=adv.manufacturer_data) 
-                        vendor = cls.DataBase.get_vendor_main(mac=mac, verbose=False) 
-                                        
-
-                        data = {
-                            "rssi": rssi,
-                            "addr": mac,
-                            "manuf": manuf,
-                            "vendor": vendor,
-                            "name": name,
-                            "uuid": uuid,
-                        }
-   
-                        
-
-                        if (mac not in cls.live_map):
-                            
             
-                            cls.live_map[mac] = {
-                                "status": "stable",
-                                "data": data,
-                                "rssi_list": [],
-                                "cycle": cycle,
-                                "unstable_d": 0,
-                                "stable_count": 0,
-                                "seen_cycles": 1,
-                                "first_seen": now,
-                                "last_seen": now
+            with Live(panel, console=console, refresh_per_second=4):
+
+                while True:
+
+
+                    await scanner.start()
+                    await asyncio.sleep(5)
+                    await scanner.stop()
+                    devices = scanner.discovered_devices_and_advertisement_data
+                    now     = time.time()
+                    cycle   += 1
+
+
+
+                    if devices: 
+                    
+                        
+                        for mac, (device, adv) in devices.items():
+                            
+                            name  = adv.local_name or False
+                            rssi  = adv.rssi
+                            uuid  = adv.service_uuids or False
+                            manuf = cls._get_manuf(manuf=adv.manufacturer_data) 
+                            vendor = cls.DataBase.get_vendor_main(mac=mac, verbose=False) 
+                                            
+
+                            data = {
+                                "rssi": rssi,
+                                "addr": mac,
+                                "manuf": manuf,
+                                "vendor": vendor,
+                                "name": name,
+                                "uuid": uuid,
                             }
+    
+                            
 
-                            cls.devices += 1
-                            console.print(f"{cls.devices}", rssi, mac, manuf, vendor, name, uuid)
-                    
+                            if (mac not in cls.live_map):
+                                
+                
+                                cls.live_map[mac] = {
+                                    "status": "stable",
+                                    "data": data,
+                                    "rssi_list": [],
+                                    "cycle": cycle,
+                                    "unstable_d": 0,
+                                    "stable_count": 0,
+                                    "seen_cycles": 1,
+                                    "first_seen": now,
+                                    "last_seen": now
+                                }
+
+                                cls.devices += 1
+                                console.print(f"{cls.devices}", rssi, mac, manuf, vendor, name, uuid)
+                        
+                            
+
+
+                            cls.live_map[mac]["rssi_list"].append(rssi)
+                            cls.live_map[mac]["seen_cycles"] += 1
+                            cls.live_map[mac]["last_seen"]   = now
+                            cls.live_map[mac]["cycle"]       = cycle
+
+                    for mac, dev in list(cls.live_map.items()):
+                            
+                        use = f"[bold red][!] {mac} ->"
+                        weight       = 0
+                        rssi_list    = dev["rssi_list"]
+                        time_missing = now - dev["last_seen"]
+
+
+                        
+                        # // C++ IS SUPERIOR
+                        if len(rssi_list) >= 3 and max(rssi_list) - min(rssi_list) > 30: 
+                            weight  += 1
+                            console.print(f"{use}[yellow] rssi spike")
+
+                        if dev["cycle"] != cycle: 
+                            weight  += 1
+                            console.print(f"{use}[yellow] missed cycle")
+
+                        if (time_missing > 10): 
+                            weight  += 2
+                            console.print(f"{use}[yellow] missing time")
                         
 
+                        if weight >= 2:
+                            if mac not in unstable_devices: 
+                                unstable_devices.append(mac)
+                            dev["status"]       = "unstable"
+                            dev["stable_count"] = 0
+                            dev["unstable_d"]   += 1
 
-                        cls.live_map[mac]["rssi_list"].append(rssi)
-                        cls.live_map[mac]["seen_cycles"] += 1
-                        cls.live_map[mac]["last_seen"]   = now
-                        cls.live_map[mac]["cycle"]       = cycle
+                            if dev["unstable_d"] > 15:
+                                unstable_devices.pop(mac)
+                            
+                        else: 
+                            if dev["status"] == "unstable":
+                                dev["stable_count"] += 1
+                                if dev["stable_count"] >= 1: 
+                                    dev["status"] = "stable"
+                                    unstable_devices.remove(mac)
+                                    console.print(f"removed: {mac}") 
 
-                for mac, dev in list(cls.live_map.items()):
-                        
-                    use          = f"[bold red][!] unstable connection  - {mac} -  Type:"
-                    weight       = 0
-                    rssi_list    = dev["rssi_list"]
-                    time_missing = now - dev["last_seen"]
 
+                        if time_missing > 30:
+                            console.print(f"[bold yellow][-] Removing stale device:[/bold yellow] {mac}")
+                            del cls.live_map[mac]
+
+
+            
 
                     
-                    # // C++ IS SUPERIOR
-                    if len(rssi_list) >= 3 and max(rssi_list) - min(rssi_list) > 20: 
 
-                    if len(rssi_list) >= 3 and max(rssi_list) - min(rssi_list) > 30: 
-                        weight  += 1
-                        console.print(f"{use}[yellow] rssi spike")
+                    # WILL MAKE A GLOBALIZED SAVE FOR ALL INFO FROM ALL MONITOR METHODS
+                    # DataBase.push_results(devices=cls.war_drive, verbose=False)
 
-                    if dev["cycle"] != cycle: 
-                        weight  += 1
-                        console.print(f"{use}[yellow] missed cycle")
-
-                    if (time_missing > 10): 
-                        weight  += 2
-                        console.print(f"{use}[yellow] missing time")
+                    
+                    count = len(devices if devices else 0)
+                    Extensions.Controller(current_count=count, server_ip=server_ip)
+                    avg   = Extensions.avg
+                    total = len(cls.live_map) or 0
+                    unstables = len(unstable_devices)
                     
 
-                    if weight >= 2:
-                        if mac not in unstable_devices: 
-                            unstable_devices.append(mac)
-                        dev["status"]       = "unstable"
-                        dev["stable_count"] = 0
-                        dev["unstable_d"]   += 1
+                    unstable_ratio = unstables / total
+                    drop_score     = (avg - count) / avg  
 
-                        if dev["unstable_d"] > 15:
-                            unstable_devices.pop(mac)
-                         
-                    else: 
-                        if dev["status"] == "unstable":
-                            dev["stable_count"] += 1
-                            if dev["stable_count"] >= 1: 
-                                dev["status"] = "stable"
-                                unstable_devices.remove(mac)
-                                console.print(f"removed: {mac}") 
+                    unstable_pct = round(unstable_ratio * 100, 2)
+                    drop_pct     = round(drop_score * 100, 2)
 
 
-                    if time_missing > 30:
-                        console.print(f"[bold yellow][-] Removing stale device:[/bold yellow] {mac}")
-                        del cls.live_map[mac]
 
-
-        
-
+                    t = unstables - total
+            
                 
+                    
+                    """
+                    console.print(
+                        f"Unstable devices: {unstables} "
+                        f"\nTotal Devices: {total}"
+                        f"\nUnstable_ratio: {unstable_pct} - Drop_score: {drop_pct}"
+                    )
+                    
+                    """
 
-                # WILL MAKE A GLOBALIZED SAVE FOR ALL INFO FROM ALL MONITOR METHODS
-                # DataBase.push_results(devices=cls.war_drive, verbose=False)
-
-                
-                count = len(devices if devices else 0)
-                Extensions.Controller(current_count=count, server_ip=server_ip)
-                avg   = Extensions.avg
-                total = len(cls.live_map) or 0
-                unstables = len(unstable_devices)
-                
-
-                unstable_ratio = unstables / total
-                drop_score     = (avg - count) / avg  
-
-                unstable_pct = round(unstable_ratio * 100, 2)
-                drop_pct     = round(drop_score * 100, 2)
-
-
-
-                t = unstables - total
-        
-             
-                
-                console.print(
-                    f"Unstable devices: {unstables} "
-                    f"\nTotal Devices: {total}"
-                    f"\nUnstable_ratio: {unstable_pct} - Drop_score: {drop_pct}"
-                )
+                    c1 = "bold yellow"
+                    panel.renderable = (f"Session Devices:[{c1}] {total}[/{c1}]  -  Unstable Devices:[{c1}] {unstables}[/{c1}]  -  Unstable Ratio:[{c1}] {unstable_pct}[/{c1}]   -  Drop Score:[{c1}] {drop_score}[/{c1}]")
 
 
 
